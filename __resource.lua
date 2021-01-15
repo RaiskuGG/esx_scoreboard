3bbff31a0289d3f4a2dcea3cfd7e3176326b6bc5
RaiskuGG <77485252+RaiskuGG@users.noreply.github.com>
2021-01-15 16:15:36 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Scoreboard'
-
-version '1.0.0'
-
 server_script 'server/main.lua'
-
 client_script 'client/main.lua'
 
 ui_page 'html/scoreboard.html'
